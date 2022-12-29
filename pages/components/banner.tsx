dad8f1bd32a95cbe48b95edd8a92d8383db522f6
humzah alkindi <humzahalkindi@hotmail.com>
2022-12-30 02:08:57 +1100

    Baseline

---
+++ pages/components/banner.tsx
@@ -0,0 +1,14 @@
+
+const Banner = () => {
+    return (
+    <header>
+            <div>
+                <img src="./GloboLogo.png" alt="logo" />    
+            </div>
+                Providing houses all over the world
+            <div></div>
+        </header>
+        );
+};
+
+export default Banner;
